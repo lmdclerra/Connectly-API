1904bfd51a5ebef96de0e2fb01bae576f4053471
Lerra Mae Dela Cruz <163080795+lmdclerra@users.noreply.github.com>
2025-02-09 17:20:53 +0800

    update validation API

--- posts/models.py
+++ posts/models.py
@@ -15,4 +15,15 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True) # Timestamp when the post was created 
 
     def __str__(self):
-        return self.content[:50]
+        # return self.content[:50]
+        return f"Post by {self.author.username} at {self.created_at}"
+    
+class Comment(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on Post {self.post.id}"
+    
--- posts/views.py
+++ posts/views.py
@@ -4,6 +4,11 @@ from .models import User
 import json
 from django.views.decorators.csrf import csrf_exempt
 from .models import Post
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User, Post, Comment
+from .serializers import UserSerializer, PostSerializer, CommentSerializer
 
 # Create your views here.
 
@@ -83,3 +88,47 @@ def create_post(request):
             return JsonResponse({'error': 'Author not found'}, status=404)
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=400)
+        
+class UserListCreate(APIView):
+    def get(self, request):
+                users = User.objects.all()
+                serializer = UserSerializer(users, many=True)
+                return Response(serializer.data)
+            
+    def post(self, request):
+                serializer = UserSerializer (data=request.data)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(serializer.data, status=status.HTTP_201_CREATED)
+                return Response(serializer.errors, status=status.HTTP_400_BAD_Request)
+
+
+class PostListCreate(APIView):
+    def get(self, request):
+          posts = Post.objects.all()
+          serializer = PostSerializer(posts, many=True)
+          return Response(serializer.data)
+     
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_Request)
+
+
+class CommentListCreate(APIView):
+    def get(self, request):
+          comments = Comment.objects.all()
+          serializer = CommentSerializer(comments, many=True)
+          return Response(serializer.data)
+     
+     
+    def post(self, request):
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+             serializer.save()
+             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
